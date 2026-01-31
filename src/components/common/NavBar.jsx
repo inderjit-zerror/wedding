8cd12bfe2c8c624156ea482952dbdd03fb1f3d31
inderjit-zerror <inderjit.zerror@gmail.com>
2026-01-31 17:47:21 +0530

    Done

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -7,8 +7,8 @@ const NavBar = () => {
         <div className='w-full h-[46px] mt-10 flex justify-between fixed top-0 left-0 items-center z-100 px-20'>
             {/* Left */}
             <div className='w-1/3 h-full items-center flex justify-between uppercase text-[18px] text-[#5F171D]'>
-               <div className='w-fit h-fit flex flex-col group relative  select-none cursor-pointer'> <div className=' absolute bottom-0 left-0 w-0 h-0.5 group-hover:w-full duration-200 ease-out bg-[#5F171D]'></div> The wedding</div>
-                <Link href={`/itinerary`}><div className='w-fit h-fit flex flex-col group relative  select-none cursor-pointer'> <div className=' absolute bottom-0 left-0 w-0 h-0.5 group-hover:w-full duration-200 ease-out bg-[#5F171D]'></div> Itinerary</div></Link>
+               <Link href={`/`}><div className='w-fit h-fit flex flex-col group relative  select-none cursor-pointer'> <div className=' absolute bottom-0 left-0 w-0 h-0.5 group-hover:w-full duration-200 ease-out bg-[#5F171D]'></div> The wedding</div></Link>
+                <Link href={`/Itinerary`}><div className='w-fit h-fit flex flex-col group relative  select-none cursor-pointer'> <div className=' absolute bottom-0 left-0 w-0 h-0.5 group-hover:w-full duration-200 ease-out bg-[#5F171D]'></div> Itinerary</div></Link>
                 <Link href={`/venue`}><div className='w-fit h-fit flex flex-col group relative  select-none cursor-pointer'> <div className=' absolute bottom-0 left-0 w-0 h-0.5 group-hover:w-full duration-200 ease-out bg-[#5F171D]'></div> VENUE</div></Link>
             </div>
 
